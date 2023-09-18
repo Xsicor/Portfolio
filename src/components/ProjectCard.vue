ac7aa3cd293d64b9311f6f72d51bef247e0f854a
Vara-U <vara.uswaransigul@g2-travel.com>
2023-09-18 21:25:17 +0100

    Change path names to work with deployment

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -2,53 +2,65 @@
 const props = defineProps({
   demoLink: {
     type: String,
-    required: true
+    required: true,
   },
   codeLink: {
     type: String,
-    required: true
+    required: true,
   },
   imageFile: {
     type: String,
-    required: true
+    required: true,
   },
   title: {
     type: String,
-    required: true
+    required: true,
   },
   description: {
     type: String,
-    required: true
+    required: true,
   },
   technologies: {
     type: Array,
-    required: true
-  }
-})
+    required: true,
+  },
+});
 </script>
 
 <template>
   <div class="card mb-12">
     <div class="card-body p-0">
       <div class="flex">
-        <img :src="`../src/assets/${imageFile}`" class="h-40 rounded object-contain" />
+        <img :src="imageFile" class="h-40 rounded object-contain" />
         <ul class="ml-4 flex flex-col justify-between py-5">
           <li>
-            <a class="group flex items-center py-3" :href="demoLink" target="_blank">
+            <a
+              class="group flex items-center py-3"
+              :href="demoLink"
+              target="_blank"
+            >
               <span
-                class="mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
+                class="mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"
+              ></span>
               <span
-                class="font-bold tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
+                class="font-bold tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200"
+              >
                 Demo
               </span>
             </a>
           </li>
           <li>
-            <a class="group flex items-center py-3" :href="codeLink" target="_blank">
+            <a
+              class="group flex items-center py-3"
+              :href="codeLink"
+              target="_blank"
+            >
               <span
-                class="mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
+                class="mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"
+              ></span>
               <span
-                class="font-bold tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
+                class="font-bold tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200"
+              >
                 Code
               </span>
             </a>
@@ -61,7 +73,9 @@ const props = defineProps({
       </p>
       <ul class="flex flex-wrap">
         <li v-for="technology in technologies" :key="technology">
-          <div class="badge badge-accent badge-lg my-2 mr-2">{{ technology }}</div>
+          <div class="badge badge-accent badge-lg my-2 mr-2">
+            {{ technology }}
+          </div>
         </li>
       </ul>
     </div>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,5 +1,8 @@
 <script setup>
-import ProjectCard from "./ProjectCard.vue"
+import ProjectCard from "./ProjectCard.vue";
+import image1 from "../assets/LondonTrainTracker.png";
+import image2 from "../assets/WebTetris.png";
+import image3 from "../assets/Diceware.png";
 </script>
 
 <template>
@@ -7,24 +10,27 @@ import ProjectCard from "./ProjectCard.vue"
     <ProjectCard
       code-link="https://github.com/Xsicor/London-Underground-Tracker"
       demo-link="https://www.youtube.com/watch?v=ivAo_4HLPi4"
-      image-file="LondonTrainTracker.png"
+      :image-file="image1"
       title="London Underground Tracker"
       description="Web app showcasing live London Underground train locations, using TFL API data for real-time
         arrivals, displaying train movements on an interactive map with animated icons"
-      :technologies="['Django', 'Mapbox', 'TFL API', 'Python', 'Javascript']" />
+      :technologies="['Django', 'Mapbox', 'TFL API', 'Python', 'Javascript']"
+    />
     <ProjectCard
       code-link="https://github.com/Xsicor/Web-Tetris"
       demo-link="https://xsicor.github.io/Web-Tetris/"
-      image-file="WebTetris.png"
+      :image-file="image2"
       title="Tetris: Block Style Editor"
       description="Tetris with a touch of customisation. This project allows you to customize block styles, developed using pure vanilla JavaScript, HTML, and CSS"
-      :technologies="['Javascript', 'HTML', 'CSS']" />
+      :technologies="['Javascript', 'HTML', 'CSS']"
+    />
     <ProjectCard
       code-link="https://github.com/Xsicor/Diceware"
       demo-link="https://xsicor.github.io/Diceware/"
-      image-file="Diceware.png"
+      :image-file="image3"
       title="Diceware: Password Generator"
       description="Create strong passwords using the Diceware method."
-      :technologies="['React', 'Javascript', 'HTML', 'CSS']" />
+      :technologies="['React', 'Javascript', 'HTML', 'CSS']"
+    />
   </div>
 </template>
